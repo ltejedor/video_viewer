6b715b4de9f1f4fecf2b838227963eb78e9a2774
Leandra Tejedor <lmtejedor@gmail.com>
2013-08-14 16:33:07 -0700

    setup api and related tests

--- app/controllers/screencasts_controller.rb
+++ app/controllers/screencasts_controller.rb
@@ -1,2 +1,15 @@
 class ScreencastsController < ApplicationController
+
+	# Get /screencasts
+	# Get /screencasts.json
+	def index
+		render json: Screencast.all
+	end
+
+	# Get /screencasts/:id
+	# Get /screencasts/:id.json
+	def show
+		render json: Screencast.find(params[:id])
+	end
+
 end
